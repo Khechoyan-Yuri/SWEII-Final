8a8cd24c534ce59760fa2c6cd2187096f8728b5c
snugglebear <vikramh50@aim.com>
2017-04-23 12:11:12 -0500

    Added SQL statements for testing

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,10 @@
 -- Insert role
-insert into role (name) values ('ROLE_USER');
+insert into role (name) values ('ROLE_ADMIN');
 
--- Insert two users
-insert into user (username,enabled,password,role_id) values ('user',true,'password',1);
-insert into user (username,enabled,password,role_id) values ('user2',true,'password',1);
+-- Insert users
+insert into user (username,enabled,password,role_id) values ('admin',true,'password',1);
 
+-- Insert customers
+insert into customer (firstname,lastname,mobilenumber,reasonforvisit,checkintime,place) values ('Vikram','Hegde','3143152856','billingpayment',CURRENT_TIMESTAMP(),0)
+insert into customer (firstname,lastname,mobilenumber,reasonforvisit,checkintime,place) values ('Yuri','Khechoyan','3123142896','techsupport',CURRENT_TIMESTAMP(),0)
+insert into customer (firstname,lastname,mobilenumber,reasonforvisit,checkintime,place) values ('Zahid','Anwar','8473123456','acctissues',CURRENT_TIMESTAMP(),0)
